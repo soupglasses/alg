0ae569e37ddcf2d5a4622384bd9a867e8e44e33e
Sofi <sofi+git@mailbox.org>
2024-03-29 09:42:10 +0100

    fix(item): simple reformat of equality test

--- src/item.c
+++ src/item.c
@@ -10,27 +10,21 @@
 
 // A non zero return value means its equal.
 int item_equal(item_t left, item_t right) {
-  switch (left.type) {
-    case 'i':
-      if (right.type != 'i') return 0;
-      return (left.data.i == right.data.i);
-    case 'd':
-      if (right.type != 'd') return 0;
-      return (left.data.d == right.data.d);
-    case 'c':
-      if (right.type != 'c') return 0;
-      return (left.data.c == right.data.c);
-    case 's':
-    case 'S':
-      if (right.type != 's' && right.type != 'S') return 0;
-      // Both `s` and `S` are defined as `char *`, so this is okay for lookup.
-      return (strcmp(left.data.s, right.data.s) == 0);
-    case 'p':
-      if (right.type != 'p') return 0;
-      return (left.data.p == right.data.p);
-  };
-  // Since we use a char, its technically possible to not match above.
-  // TODO: Handle this better, dont just say its not equal.
+  if (left.type == right.type || ((left.type == 's' || left.type == 'S') && (right.type == 's' || right.type == 'S')))
+    switch (left.type) {
+      case 'i':
+        return (left.data.i == right.data.i);
+      case 'd':
+        return (left.data.d == right.data.d);
+      case 'c':
+        return (left.data.c == right.data.c);
+      case 's':
+      case 'S':
+        // Both `s` and `S` are defined as `char *`, so this is okay for lookup of either.
+        return (strcmp(left.data.s, right.data.s) == 0);
+      case 'p':
+        return (left.data.p == right.data.p);
+    };
   return 0;
 }
 
